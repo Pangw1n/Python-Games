567427e0e0898feda358f4805b7ee910e10fcb30
Pangw1n <97049344+Pangw1n@users.noreply.github.com>
2025-07-25 04:01:10 +0000

    added camera handling

--- lessons/07_Projects/Doodle_Jump/main.py
+++ lessons/07_Projects/Doodle_Jump/main.py
@@ -1,42 +1,72 @@
 import pygame
 
-#SETTINGS
-class GameSettings:
-    SCREEN_WIDHT = 300
-    SCREEN_HEIGHT = 400
-    PLAYER_SIZE = 35
-    PLATFORM_WIDTH = 50
+#SETTING
+SCREEN_WIDHT = 300
+SCREEN_HEIGHT = 400
+PLAYER_SIZE = 35
+PLATFORM_WIDTH = 50
 PLATFORM_THICKNESS = 20
 SPEED = 20
-GRAVITY = 1
+GRAVITY = -1
 GAME_SPEED = 15
-camera_y_pos = 0
-white: tuple = (255, 255, 255)
-black: tuple = (0, 0, 0)
+MAX_Y_DIFF = 200
+WHITE: tuple = (255, 255, 255)
+BLACK: tuple = (0, 0, 0)
 
-# Initialize Pygame
-pygame.init()
+class Game:
+    def __init__(self):
+        # Initialize Pygame
+        pygame.init()
 
-# Initialize screen
-screen = pygame.display.set_mode((SCREEN_WIDHT, SCREEN_HEIGHT))
+        # Initialize screen
+        self.screen = pygame.display.set_mode((SCREEN_WIDHT, SCREEN_HEIGHT))
+        self.cam_y_pos = 0
+
+    def start_game(self):
+        # Define player
+        self.player = Player()
+        self.platforms = list()
+        self.platforms.append(Platform((SCREEN_WIDHT - PLATFORM_WIDTH) / 2, PLATFORM_THICKNESS + 20))
+
+        self.running = True
+        self.clock = pygame.time.Clock()
+
+        while self.running:
+            self.player.update(self)
+
+            # Draw everything
+            self.screen.fill(WHITE)
+            self.player.draw(self.screen)
+            for x in self.platforms:
+                x.update(self)
+                x.draw(self.screen)
+
+            pygame.display.flip()
+            self.clock.tick(30)
+        pygame.quit
+    
+    def y_to_screen(self, y):
+        return SCREEN_HEIGHT - (y - self.cam_y_pos)
 
 class Player:
     def __init__(self):
         self.x_vel = 0
-        self.y_vel = -16
+        self.y_vel = 16
         self.x = (SCREEN_WIDHT - PLAYER_SIZE) / 2
-        self.y = SCREEN_HEIGHT - PLAYER_SIZE - PLATFORM_THICKNESS - 20
+        self.y = PLAYER_SIZE + PLATFORM_THICKNESS + 20
         self.hitbox = pygame.Rect(self.x, self.y, PLAYER_SIZE, PLAYER_SIZE)
 
-    def update(self):
+    def update(self, game):
         self.y_vel += GRAVITY
         self.x += self.x_vel
         self.y += self.y_vel
+        if self.y > game.cam_y_pos + MAX_Y_DIFF:
+            game.cam_y_pos = self.y - MAX_Y_DIFF
         self.hitbox.x = self.x
-        self.hitbox.y = self.y
+        self.hitbox.y = game.y_to_screen(self.y)
         
-    def draw(self):
-        pygame.draw.rect(screen, black, self.hitbox)
+    def draw(self, screen):
+        pygame.draw.rect(screen, BLACK, self.hitbox)
 
 class Platform:
     def __init__(self, x, y):
@@ -44,32 +74,13 @@ class Platform:
         self.y = y
         self.hitbox = pygame.Rect(self.x, self.y, PLATFORM_WIDTH, PLATFORM_THICKNESS)
 
-    def update(self):
+    def update(self, game):
         self.hitbox.x = self.x
-        self.hitbox.y = self.y
+        self.hitbox.y = game.y_to_screen(self.y)
 
-    def draw(self):
-        pygame.draw.rect(screen, black, self.hitbox)
+    def draw(self, screen):
+        pygame.draw.rect(screen, BLACK, self.hitbox)
 
-# Define player
-player = Player()
-platforms = list()
-platforms.append(Platform((SCREEN_WIDHT - PLATFORM_WIDTH) / 2, SCREEN_HEIGHT - PLATFORM_THICKNESS - 20))
-
-running = True
-clock = pygame.time.Clock()
-
-
-while running:
-    player.update()
-
-    # Draw everything
-    screen.fill(white)
-    player.draw()
-    for x in platforms:
-        x.draw()
-
-    pygame.display.flip()
-    clock.tick(30)
-
-pygame.quit
+game = Game()
+game.__init__()
+game.start_game()
